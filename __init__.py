049a1c1f52d9cf7ac87088ca3fef5904cb9bb12d
TokinoChanshi <104444449+TokinoChanshi@users.noreply.github.com>
2025-10-18 17:41:17 +0800

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Koalas Circles ComfyUI 插件
-
-将图像转换为圆形像素点表示，支持标准模式和随机模式。
-"""
-
-from .koalas_circles import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
